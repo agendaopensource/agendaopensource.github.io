37d5b14f4bf50ef3b3280dcaf3477928eb7691c5
Tiago Lopes <tiagompglopes@gmail.com>
2018-02-10 07:01:35 +0000

    Add continuous integration script

---
+++ assets/ci-build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# skip if build is triggered by pull request
+if [ $TRAVIS_PULL_REQUEST == "true" ]; then
+  echo "this is PR, exiting"
+  exit 0
+fi
+
+# enable error reporting to the console
+set -e
+
+# cleanup "_site"
+rm -rf _site
+mkdir _site
+
+# clone remote repo to "_site"
+git clone https://${GITHUB_TOKEN}@github.com/agendaopensource/agendaopensource.github.io.git --branch gh-pages _site
+# build with Jekyll into "_site"
+bundle exec jekyll build
+
+# push
+cd _site
+git config user.email "travis-ci@agendaopensource.github.io"
+git config user.name "Travis CI"
+git add --all
+git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
+git push --force origin gh-pages
